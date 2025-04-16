13d8b590b5d9021ae7bd0158dfdeaecc2bb6ade4
Matt Raible <matt.raible@crowdstrike.com>
2025-04-16 13:10:00 -0600

    Initial import of Enrich Incidents app

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# foundry-tutorial-enrich-incidents
+# Enrich Incidents tutorial Foundry app
+
+This repository tracks the Enrich Incidents app export that was created based on the [Create an App that Enriches Falcon Incidents](https://falcon.crowdstrike.com/documentation/page/h3b99cc8/create-an-app-that-enriches-falcon-detections) tutorial.
---
+++ ui/extensions/Third-party Detections/src/routes/home.js
@@ -0,0 +1,55 @@
+import React, { useContext, useEffect, useState } from "react";
+import { FalconApiContext } from "../contexts/falcon-api-context";
+import { Link } from '../components/link';
+
+function Home() {
+  const {falcon} = useContext(FalconApiContext);
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const apiIntegration = falcon.apiIntegration({
+          definitionId: '6afd658618c8400eb5136d4fb019b527',
+          operationId: 'GET__posts'
+        });
+        const response = await apiIntegration.execute();
+        setData(response.resources[0].response_body);
+        console.log('data', data);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div className="mt-4 space-y-8">
+      <p class="text-neutral"> 👋 Hi {falcon.data.user.username}</p>
+      <Link useFalconNavigation={true} to="/crowdscore">CrowdScore</Link>
+
+      <p>Results from JSON Placeholder API:</p>
+
+      <ul>
+        {data.map(item => (
+          <li key={item.id}>{item.id}: {item.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export { Home };
